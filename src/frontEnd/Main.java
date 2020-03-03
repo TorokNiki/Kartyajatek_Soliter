d4bb23513089c90470e7633d4e6e5a32de34fc0e
TorokNiki <niki.torok@gmail.com>
2020-03-03 12:17:13 +0100

    refaktor

--- src/frontEnd/Main.java
+++ src/frontEnd/Main.java
@@ -1,36 +1,9 @@
 package frontEnd;
 
-import backEnd.services.factory.Config;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import frontEnd.Indito;
 
-
-public class Main extends Application {
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        Indito.main(args);
     }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Config.loadProp();
-        MainController mainController= new MainController(primaryStage);
-//        MenuBar menuBar = OwnMenu.getMenuBar(primaryStage);
-//        this.score = new TextField();
-//        OwnMenu m = new OwnMenu();
-//        MainController c = new MainController();
-//        Pane first = c.upsideDownPyramid();
-//        score=m.setTFScore(c.getScore(), c.getCurrentScore());
-//        this.gameName = new Label();
-//        m.setLabel(gameName,"Upside-Down Piramid (1/10)","#752777");
-//        Group root = new Group(menuBar, score, gameName, first);
-//        primaryStage.getIcons().add(new Image("./resources/img/ace.png"));
-//        primaryStage.setTitle("Kártyajáték (Solitaire)");
-//        primaryStage.setScene(new Scene(root, 800, 624));
-//        primaryStage.setResizable(false);
-//        primaryStage.show();
-    }
-
-
-
 }
